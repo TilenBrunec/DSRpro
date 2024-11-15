a49fd53502308db90d314a83ed5028554d6be6f4
TilenBrunec <tilen.brunec@student.um.si>
2024-11-15 23:59:27 +0100

    login-register, navbar

--- index.php
+++ index.php
@@ -8,13 +8,58 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Quotify</title>
 </head>
 <body>
+<nav>
+      <div class="logo">
+        <img src="picture/Logo64x64.png" alt="logo" />
+        <h1>LOGO</h1>
+      </div>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Blog</a>
+        </li>
+        <li>
+          <a href="#">Contact Us</a>
+        </li>
+      </ul>
+      <div class="hamburger">
+        <span class="line"></span>
+        <span class="line"></span>
+        <span class="line"></span>
+      </div>
+    </nav>
+    <div class="menubar">
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Blog</a>
+        </li>
+        <li>
+          <a href="#">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+
+
+
+
 
     <h1>Welcome to Quotify.</h1>
-    
+    <script src="js/script.js"></script>
 </body>
 </html>
 
