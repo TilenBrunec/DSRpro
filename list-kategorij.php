4fa1f0b7c068c7680c512209a8d6f1985fc573f1
TilenBrunec <tilen.brunec@student.um.si>
2024-11-16 20:58:17 +0100

    nisi se dokoncal

--- list-kategorij.php
+++ list-kategorij.php
@@ -72,6 +72,42 @@
 
 
     <h1>Welcome to Quotify.</h1>
+    <!-- 
+============================================================
+=                                                          =
+=                         FOOTER                            =
+=                                                          =
+============================================================
+-->
+<footer>
+        <div class="container-footer-all">
+            <div class="footer-content">
+                <h3>Contact Us</h3>
+                <p>Email:Info@example.com</p>
+                <p>Phone:+121 56556 565556</p>
+                <p>Address:Your Address 123 street</p>
+            </div>
+            <div class="footer-content">
+                <h3>Quick Links</h3>
+                 <ul class="list-footer">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Services</a></li>
+                    <li><a href="">Products</a></li>
+                    <li><a href="">Contact</a></li>
+                 </ul>
+            </div>
+            <div class="footer-content">
+                <h3>Follow Us</h3>
+                <ul class="social-icons">
+                  
+                </ul>
+                </div>
+        </div>
+        <div class="bottom-bar">
+            <p>&copy; 2023 your company . All rights reserved</p>
+        </div>
+    </footer>
     <script src="js/script.js"></script>
 </body>
 </html>
