f85334df71c3992ff1921bf2615a2f0c90100317
TilenBrunec <tilen.brunec@student.um.si>
2024-11-16 17:49:06 +0100

    nisi se dokoncal

--- login-register.php
+++ login-register.php
@@ -9,8 +9,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <link rel="stylesheet" href="css/login-register.css">
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="css/login-register.css">
     <link rel="stylesheet" href="css/style.css">
     <title>Quotify</title>
 </head>
